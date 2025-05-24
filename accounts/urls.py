484eaf035049da5dce16045ad1ef5738c6df0128
Shivansh Jaiswal <37285384+sjais1337@users.noreply.github.com>
2025-05-25 04:28:02 +0530

    Update urls.py
    Fixed minor warning.

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('/register', views.register, name='register'),
+    path('register/', views.register, name='register'),
     path('', include('django.contrib.auth.urls'))
 ]
